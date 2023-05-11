e0b18cf769061f315b1019b2665544c5f5a74b65
annikiy.skryabin <annikiy.skryabin@simpleone.ru>
2023-05-11 22:06:31 +0200

    New

---
+++ skryabin-annikiy/javaScript/script.js
@@ -0,0 +1,27 @@
+/*let message;
+message= 'Hello world!';
+console.log(message);
+
+let user = 'John',
+    age ='25',
+    messageTwo = 'Hello world!';
+console.log(user, age, messageTwo);
+
+messageTwo = 'YO'
+console.log(messageTwo);
+
+
+const myBirthDay = '1993';
+console.log(myBirthDay);\
+
+
+ */
+
+let admin,
+    name;
+name = 'Джон'
+admin = name;
+console.log(admin);
+
+let planetaZemlya,
+    firstNameUser;
