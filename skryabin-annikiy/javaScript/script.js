7f9a7c7c270c41849d6fa7b69d0849b74c42f6cf
annikiy.skryabin <annikiy.skryabin@simpleone.ru>
2023-05-14 19:12:34 +0200

    inProgress

--- skryabin-annikiy/javaScript/script.js
+++ skryabin-annikiy/javaScript/script.js
@@ -1,27 +1,59 @@
-/*let message;
-message= 'Hello world!';
-console.log(message);
+/*
+// let message;
+// message= 'Hello world!';
+// console.log(message);
 
-let user = 'John',
-    age ='25',
-    messageTwo = 'Hello world!';
-console.log(user, age, messageTwo);
+// let user = 'John',
+//     age ='25',
+//     messageTwo = 'Hello world!';
+// console.log(user, age, messageTwo);
 
-messageTwo = 'YO'
-console.log(messageTwo);
-
-
-const myBirthDay = '1993';
-console.log(myBirthDay);\
+// messageTwo = 'YO'
+// console.log(messageTwo);
 
 
- */
+// const myBirthDay = '1993';
+// console.log(myBirthDay);\
 
-let admin,
-    name;
-name = 'Джон'
-admin = name;
-console.log(admin);
+// let admin,
+//     name;
+// name = 'Джон'
+// admin = name;
+// console.log(admin);
 
-let planetaZemlya,
-    firstNameUser;
+// let planetaZemlya,
+//     firstNameUser;
+
+*/
+// Типы данных
+
+    // Примитивные
+        // 1. nuber
+        // 2. BigInt
+        // 3. string
+        // 4. Boolean
+        // 5.
+        // 6.
+        // 7.
+
+    // Не примитивные
+        // 1. Object
+
+// Специальные числовые значения
+    // infinity
+        let results = 1 / 0;
+        console.log(results); // infinity
+
+    // NaN
+    results = 'a' / 2;
+    console.log(results); // NaN
+
+
+
+console.log('gg');
+
+
+let name = 'King';
+console.log(`Привет ${name}!`);
+name = 'Queen';
+console.log('Привет ' + name + '!');
