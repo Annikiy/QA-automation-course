e249179625f2c1c09cbd96b3d5c65b3818da79e8
annikiy.skryabin <annikiy.skryabin@simpleone.ru>
2023-05-22 13:40:53 +0200

    imPss

--- skryabin-annikiy/javaScript/script.js
+++ skryabin-annikiy/javaScript/script.js
@@ -1,45 +1,166 @@
+// Логические операторы
+// 1. || или
+// 2. && и
+// 3. ! не
+// 4. ?? оператор нулевого слиняние
+
+
+
+
+
+
+
+// Операторы сравнения
+    // > больше 
+    // < меньше
+    // >= больше равно
+    // <= меньше равно
+    // == равно
+    // != не равно
+    
+    // возвращают Boolean
+
+    // можно  сравнивать строки
+    // console.log('A' < 'Я') вернёт true, так-как 'Я' последняя в алфовите
+    // console.log('коты' > 'кода') вернёт true, сравнивает посимвольно 
+    // console.log('Ф' > 'ф') вернёт false, 
+    // null == undefined // true
+    // null === undefined // false
+
+
+// if
+    // const number = 12;
+    // if (number === 12) console('равно');
+    // // не скоращенный и рекомендованный вариант:
+    // if (number === 12) {
+    //     console('равно');
+    //     console('точно 12');
+    // };
+
+    // let age = 20;
+    //     if (age < 18) {
+    //         accesessAllowed = false
+    //     } else {
+    //         accesessAllowed = true
+    //     };
+
+    //const result = условие ? значение1 : знаечние2
+        // let = age = 20;
+        // let accesessAllowed = age < 18 ? false : true;
+        // let accesessAllowed = age < 18 ? 'нет' : 'да';
+
+            // Где:
+                // ? - это if
+                // : - это else
+
+
+
+
+
+
+
+
+
+// Операнд
+    // левый * правый операнд
+    // 5 * 2
+
+// Унарный
+    // const x = 1;
+    // console.log(-x); // -1 унарный минус
+
+// Бинарный
+    // const x = 5;
+    // const y = 3;
+    // console.log(x - y) // 2 бинарный минус
+
+
+// Математические операторы 
+    // +
+    // -
+    // *
+    // .
+    // %
+    // **
+
+// инкримент
+    // let counter = 3;
+    // counter++
+    // console.log(counter)
+
+// декримент
+    // let counter = 3;
+    // counter--
+    // console.log(counter)
+
+// // префиксное значение
+//     let counter = 1;
+//     console.log(
+//         ++counter
+//         )
+
+// // постфиксное значение
+//     console.log(
+//         counter++
+//         )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
-// let message;
-// message= 'Hello world!';
-// console.log(message);
+let message;
+message= 'Hello world!';
+console.log(message);
 
-// let user = 'John',
-//     age ='25',
-//     messageTwo = 'Hello world!';
-// console.log(user, age, messageTwo);
+let user = 'John',
+    age ='25',
+    messageTwo = 'Hello world!';
+console.log(user, age, messageTwo);
 
-// messageTwo = 'YO'
-// console.log(messageTwo);
+messageTwo = 'YO'
+console.log(messageTwo);
 
 
-// const myBirthDay = '1993';
-// console.log(myBirthDay);\
+const myBirthDay = '1993';
+console.log(myBirthDay);\
 
-// let admin,
-//     name;
-// name = 'Джон'
-// admin = name;
-// console.log(admin);
+let admin,
+    name;
+name = 'Джон'
+admin = name;
+console.log(admin);
 
-// let planetaZemlya,
-//     firstNameUser;
-
+let planetaZemlya,
+    firstNameUser;
 */
-// Типы данных
+/* Типы данных
 
-    // Примитивные
-        // 1. nuber
-        // 2. BigInt
-        // 3. string
-        // 4. Boolean
-        // 5.
-        // 6.
-        // 7.
+    Примитивные
+        1. nuber
+        2. BigInt
+        3. string
+        4. Boolean
+        5.
+        6.
+        7.
 
-    // Не примитивные
-        // 1. Object
+    Не примитивные
+        1. Object
 
-// Специальные числовые значения
+Специальные числовые значения
     // infinity
         let results = 1 / 0;
         console.log(results); // infinity
@@ -57,3 +178,4 @@ let name = 'King';
 console.log(`Привет ${name}!`);
 name = 'Queen';
 console.log('Привет ' + name + '!');
+*/
