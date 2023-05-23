4034549fac351684f709f82ad96b164f0fd82f32
annikiy.skryabin <annikiy.skryabin@simpleone.ru>
2023-05-24 00:00:03 +0200

    js-hw-12-done-1-2-3

--- skryabin-annikiy/javaScript/hw-12-script.js
+++ skryabin-annikiy/javaScript/hw-12-script.js
@@ -1,11 +1,48 @@
+//1.
+console.log('Task 1')
 let i;
 let j;
-let text;
-lit
+let str = '';
+for (i = 0; i < 3; i++) {
+  for (j = 0; j < 3; j++) {
+    str += '*';
+  }
+  str += '_';
+}
+console.log(str); // ***_***_***_
+console.log('-------------------');
 
-for (i; i == 12; i += 1){
-    for(j; j === '3'; j +=){
 
+//2.
+console.log('-------------------\n\nTask 2\n-------------------');
+str = '';
+for(i = 1; i < 4; i++){
+
+    str += `${i}\n`;
+
+    for(j = 0; j < 3; j++){
+
+        str += '*_';
     }
 
+    str += '\n';
 }
+console.log(str); // good
+console.log('-------------------');
+
+
+//3.
+console.log('-------------------\n\nTask 3\n-------------------');
+str = '';
+
+for (i = 0; i < 3; i++){
+
+    for(j = 0; j < 3; j++){
+
+        str += '*_';
+    }
+
+    str += '\n';
+}
+console.log(str); // good
+console.log('-------------------');
